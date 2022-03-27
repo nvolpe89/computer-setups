0e7f8edc73edf5e1262d32b0a9e7d19f200683e4
nvolpe89 <58093808+nvolpe89@users.noreply.github.com>
2022-03-27 12:46:36 -0400

    Add files via upload

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -5,26 +5,39 @@ dnf check-update
 sudo dnf upgrade --refresh -y
 
 # install material shell
-sudo dnf install gnome-shell-extension-material-shell
+sudo dnf install gnome-shell-extension-material-shell -y
 
 # enable material shell
 gnome-extensions enable material-shell@papyelgringo
 
-# INSTALL SOFTWARE
+#######################
+## INSTALL DEPENDANCIES
+#######################
+
+# add flathub repo to flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+###################
+## INSTALL SOFTWARE
+###################
 
 # install brave browser
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
+sudo dnf install brave-browser -y
 
 # install telegram
-flatpak install flathub org.telegram.desktop
+flatpak install flathub org.telegram.desktop -y
 
 # install 1password
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-sudo dnf install 1password
+sudo dnf install 1password -y
+
+# install howdy
+sudo dnf copr enable principis/howdy
+sudo dnf --refresh install howdy -y
 
 #reboot system
 sudo shutdown -r now
